d04c4287260c0029c1db9a9c01f6b533999d565d
Kre4 <denhol2002@gmail.com>
2025-03-06 10:03:31 +0300

    feat: add dsl marker; fix: fix rewrite if both exists and new block are declared

--- repository-dsl/src/main/kotlin/ru/kre4/repository/extensions/CrudRepositoryUpdateIfExistsOrSaveExtension.kt
+++ repository-dsl/src/main/kotlin/ru/kre4/repository/extensions/CrudRepositoryUpdateIfExistsOrSaveExtension.kt
@@ -1,9 +1,11 @@
 package ru.kre4.repository.extensions
 
 import org.springframework.data.repository.CrudRepository
+import ru.kre4.repository.extensions.common.EmptyContext
 
+@Target(AnnotationTarget.CLASS, AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.FUNCTION, AnnotationTarget.TYPE)
 @DslMarker
-annotation class SaveUpdate
+annotation class SaveUpdateMarker
 
 /**
  * Extension function that provides DSL language for updating existing entity or providing new one for save
@@ -15,7 +17,8 @@ fun <T, ID : Any, R> R.saveOrUpdate(init: SaveUpdateContext<T, ID, R>.() -> Unit
     )
 }
 
-open class SaveUpdateContext<T, ID : Any, R>(private val repository: R) where R : CrudRepository<T, ID> {
+@SaveUpdateMarker
+class SaveUpdateContext<T, ID : Any, R>(private val repository: R) where R : CrudRepository<T, ID> {
     private var _entity: T? = null
     val entity: T?
         get() = _entity
@@ -24,16 +27,17 @@ open class SaveUpdateContext<T, ID : Any, R>(private val repository: R) where R 
         _entity = repository.findById(id).get() // todo give more API for better user experience
     }
 
-    fun bySearch(init: R.() -> T?) {
+    fun bySearch(init: (@SaveUpdateMarker R).() -> T?) {
         _entity = repository.init()
     }
 
-    fun ifExists(init: T.() -> Unit) {
+    fun ifExists(init: (@SaveUpdateMarker T).() -> Unit) {
         _entity?.apply(init)
     }
 
-    fun ifNew(init: () -> T) {
-        _entity = init.invoke()
+    fun ifNew(init: (@SaveUpdateMarker EmptyContext).() -> T) {
+        if (_entity == null)
+            _entity = EmptyContext().init()
     }
 
 }
---
+++ repository-dsl/src/main/kotlin/ru/kre4/repository/extensions/common/EmptyContext.kt
@@ -0,0 +1,3 @@
+package ru.kre4.repository.extensions.common
+
+class EmptyContext
