11efd4ca1547429fe7116e734eff47d72eff07ae
Kre4 <denhol2002@gmail.com>
2025-03-09 17:32:53 +0300

    feat: add lazy calculations to keep correct order of calls

--- repository-dsl/src/main/kotlin/ru/kre4/repository/extensions/CrudRepositoryUpdateIfExistsOrSaveExtension.kt
+++ repository-dsl/src/main/kotlin/ru/kre4/repository/extensions/CrudRepositoryUpdateIfExistsOrSaveExtension.kt
@@ -1,43 +1,61 @@
 package ru.kre4.repository.extensions
 
 import org.springframework.data.repository.CrudRepository
-import ru.kre4.repository.extensions.common.EmptyContext
+import java.util.*
 
 @Target(AnnotationTarget.CLASS, AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.FUNCTION, AnnotationTarget.TYPE)
 @DslMarker
 annotation class SaveUpdateMarker
 
+
 /**
  * Extension function that provides DSL language for updating existing entity or providing new one for save
  */
-fun <T, ID : Any, R> R.saveOrUpdate(init: SaveUpdateContext<T, ID, R>.() -> Unit): T where R : CrudRepository<T, ID> {
-    val context = SaveUpdateContext(this).apply(init)
-    return this.save(
-        context.entity ?: throw KotlinNullPointerException("No function for selecting entity was called. CheckIf")
-    )
+fun <T, ID : Any, R> R.saveUpdate(init: SaveUpdateContext<T, ID, R>.() -> Unit): T where R : CrudRepository<T, ID>, T : Any {
+    val context = SaveUpdateContextEntityCalculator(this).apply(init)
+    return this.save(context.calculateEntity())
 }
 
 @SaveUpdateMarker
-class SaveUpdateContext<T, ID : Any, R>(private val repository: R) where R : CrudRepository<T, ID> {
-    private var _entity: T? = null
-    val entity: T?
-        get() = _entity
+open class SaveUpdateContext<T, ID : Any, R>(private val repository: R) where R : CrudRepository<T, ID>, T : Any {
+    protected var _entity: Optional<T> = Optional.empty()
+    protected lateinit var ifExistsConsumer: ((T).() -> Unit)
+    protected lateinit var ifNewSupplier: ((EmptyContext).() -> T)
+
+    protected val entity: T?
+        get() = _entity.orElse(null)
 
     fun byId(id: ID) {
-        _entity = repository.findById(id).get() // todo give more API for better user experience
+        _entity = repository.findById(id)
     }
 
     fun bySearch(init: (@SaveUpdateMarker R).() -> T?) {
-        _entity = repository.init()
+        _entity = Optional.ofNullable(repository.init())
     }
 
     fun ifExists(init: (@SaveUpdateMarker T).() -> Unit) {
-        _entity?.apply(init)
+        ifExistsConsumer = init
     }
 
     fun ifNew(init: (@SaveUpdateMarker EmptyContext).() -> T) {
-        if (_entity == null)
-            _entity = EmptyContext().init()
+        ifNewSupplier = init
     }
-
 }
+
+private class SaveUpdateContextEntityCalculator<T, ID : Any, R>(private val repository: R) :
+    SaveUpdateContext<T, ID, R>(repository) where R : CrudRepository<T, ID>, T : Any {
+
+    fun calculateEntity(): T {
+        return _entity.map {
+            it.ifExistsConsumer()
+            it
+        }.orElseGet {
+            EmptyContext().ifNewSupplier()
+        }
+    }
+}
+
+/**
+ * Fake context for correct DSL usage in Supplier-like functions ( () -> T )
+ */
+class EmptyContext
