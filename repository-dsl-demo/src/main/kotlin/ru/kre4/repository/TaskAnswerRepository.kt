72b9f0c12ef1343a460bdecfeec69360255bf81e
Kre4 <denhol2002@gmail.com>
2025-03-19 19:43:30 +0300

    feat: simplifying demonstration by removing service layer

--- repository-dsl-demo/src/main/kotlin/ru/kre4/repository/TaskAnswerRepository.kt
+++ repository-dsl-demo/src/main/kotlin/ru/kre4/repository/TaskAnswerRepository.kt
@@ -1,10 +1,15 @@
 package org.example.ru.kre4.repository
 
 import org.example.ru.kre4.entity.TaskAnswer
+import org.springframework.data.jdbc.repository.query.Query
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
 interface TaskAnswerRepository: CrudRepository<TaskAnswer, Int> {
     fun findByTaskNumberAndStudentId(taskNumber: Int, studentId: Int): TaskAnswer?
+
+    @Query("select * from task_answers where task_number = :taskNumber and student_id = :studentId")
+    fun findByTaskNumberAndStudentIdOptional(taskNumber: Int, studentId: Int): Optional<TaskAnswer>
 }
--- repository-dsl-demo/src/main/kotlin/ru/kre4/rest/api/TaskAnswerController.kt
+++ repository-dsl-demo/src/main/kotlin/ru/kre4/rest/api/TaskAnswerController.kt
@@ -1,16 +1,60 @@
 package org.example.ru.kre4.rest.api
 
+import org.example.ru.kre4.entity.TaskAnswer
+import org.example.ru.kre4.repository.TaskAnswerRepository
 import org.example.ru.kre4.rest.dto.SaveAnswerDto
-import org.example.ru.kre4.service.TaskAnswerService
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import ru.kre4.repository.extensions.saveUpdate
 
 @RestController
 @RequestMapping("/api/v1/task-answer")
-class TaskAnswerController(private val taskAnswerService: TaskAnswerService) {
+class TaskAnswerController(private val taskAnswerRepository: TaskAnswerRepository) {
 
-    @PostMapping
-    fun save(@RequestBody dto: SaveAnswerDto) = taskAnswerService.save(dto)
+    @PostMapping("/kotlin-dsl")
+    fun save(@RequestBody dto: SaveAnswerDto): TaskAnswer {
+        return taskAnswerRepository.saveUpdate {
+            bySearch {
+                findByTaskNumberAndStudentId(dto.taskNumber, dto.studentId)
+            }
+            ifExists {
+                savedAnswer = dto.answer
+            }
+            ifNew {
+                TaskAnswer(
+                    studentId = dto.studentId,
+                    savedAnswer = dto.answer,
+                    taskNumber = dto.taskNumber
+                )
+            }
+        }
+    }
+
+    @PostMapping("/kotlin-native")
+    fun saveShortButUgly(@RequestBody dto: SaveAnswerDto): TaskAnswer {
+        return taskAnswerRepository.save(
+            taskAnswerRepository.findByTaskNumberAndStudentId(dto.taskNumber, dto.studentId)?.also {
+                it.savedAnswer = dto.answer
+            } ?: TaskAnswer(
+                studentId = dto.studentId,
+                savedAnswer = dto.answer,
+                taskNumber = dto.taskNumber
+            ))
+    }
+
+    @PostMapping("/java-style")
+    fun saveViaOptional(@RequestBody dto: SaveAnswerDto): TaskAnswer {
+        return taskAnswerRepository.save(
+            taskAnswerRepository.findByTaskNumberAndStudentIdOptional(dto.taskNumber, dto.studentId)
+                .orElse(
+                    TaskAnswer(
+                        studentId = dto.studentId,
+                        savedAnswer = dto.answer,
+                        taskNumber = dto.taskNumber
+                    )
+                )
+        )
+    }
 }
